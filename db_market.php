1bd9490b0ca5da572bb34f048622e01b507b69ad
Mitch Wilson <Mitch Wilson>
2017-01-31 10:54:28 -0500

    moved db file, updated header

--- db_market.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conn_market = mysqli_connect('localhost', 'root', 'Il2g2DW!', 'market');
-
-if ($conn_market->connect_error) {
-	die('Connection Failed:'.$conn_market->connect_error);
-}
--- header.php
+++ header.php
@@ -1,5 +1,4 @@
 <?php
 
-include 'db_market.php';
-include 'scraper/simple_html_dom.php';
+include '../db_market.php';
 include 'globals.php';
