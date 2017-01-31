665edbea6be13492b1ba32597e8d50378a208598
Mitch Wilson <Mitch Wilson>
2017-01-31 11:02:14 -0500

    removed api key

--- import.php
+++ import.php
@@ -3,24 +3,14 @@
 include 'header.php';
 ini_set("allow_url_fopen", 1);
 
-//$p = '270';
-//$url = 'http://steamcommunity.com/market/search?q=&category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=any&category_730_Quality%5B%5D=tag_normal&category_730_Quality%5B%5D=tag_strange&category_730_Quality%5B%5D=tag_unusual&category_730_Quality%5B%5D=tag_unusual_strange&category_730_Type%5B%5D=tag_CSGO_Type_Pistol&category_730_Type%5B%5D=tag_CSGO_Type_SMG&category_730_Type%5B%5D=tag_CSGO_Type_Rifle&category_730_Type%5B%5D=tag_CSGO_Type_SniperRifle&category_730_Type%5B%5D=tag_CSGO_Type_Shotgun&category_730_Type%5B%5D=tag_CSGO_Type_Machinegun&category_730_Type%5B%5D=tag_CSGO_Type_Knife&appid=730#p'.$p.'_price_asc';
-//echo $url;
-// $html = file_get_html($url);
-// foreach ($html->find('a[class=market_listing_row_link]') as $link) {
-	// echo $link;	
-// }
-
 /* */
-$json = file_get_contents('csgo-skins.txt');
-//$json = file_get_contents('http://api.csgo.steamlytics.xyz/v1/items?key=8fd6f5bfc4065f39e8be25292eac7ceb');
+//$json = file_get_contents('csgo-skins.txt');
 $arrSkins = json_decode($json);
-//print_r($arrSkins);
 foreach ($arrSkins->items AS $skin) {
 	$name = $conn_market->real_escape_string($skin->market_name);
 	$url = urlencode($name);
 	$url = str_replace('+','%20',$url);
-	if ((substr($url, -3) == '%29' || substr($url,0,9) != '%E2%98%85') && !strpos($name, 'Souvenir')) {
+	if ((substr($url, -3) == '%29' || substr($url,0,9) != '%E2%98%85') && !strpos($name, 'Souvenir')) {  // These cases should separate out only non souvenir gun skins
 		$arrClean[$url] = $name;
 	} 
 }
@@ -31,46 +21,6 @@ while($row = $result->fetch_assoc()) {
 	$arrNotEmpty[] = $row['link'];
 }
 
-	// $ch = curl_init();
-	// $file = $GLOBALS['csgo_item_link'].$url;
-	// curl_setopt($ch, CURLOPT_URL, $file);
-	// curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-	// $store = curl_exec($ch);
-	// curl_close($ch);
-	// $doc = new DOMDocument();
-	// $doc->loadHTML($store);
-	//var_dump($doc);
-	// $text = $doc->textContent;
-	// $a = substr($text,strpos($text,'var g_rgAppContextData'));
-	// $raw = explode('var ',$a);
-	//print_r($raw);
-	// $avg = 0;
-	// foreach ($raw AS $var) {
-		// $expl = explode('=',$var);
-		// if (isset($expl[0]) && isset($expl[1])) {
-			// if ($decode = json_decode(rtrim(rtrim($expl[1]),';'))) {
-				// $jsScrape[$expl[0]] = $decode;
-			// } else {
-				// $jsScrape[$expl[0]] = $expl[1];
-			// }				
-			// if ($expl[0] == 'line1') {
-				// $history = explode('],[',substr($expl[1],2,(strpos($expl[1],']]')-2)));
-				// $price = $count = 0;
-				// foreach ($history AS $entry) {
-					// $array = explode(',',$entry);
-					// $date = substr($array[0],1,(strpos($array[0],':')-3));
-					//echo $date.' '.$array[2];
-					// if (strtotime($date) >= strtotime('- 30 days')) {
-						// $c = trim($array[2],'"');
-						// $price += $array[1]*$c;
-						// $count += $c;
-					// }
-				// }
-				// $avg = '$'.number_format(round(($price/$count),2), 2, '.', ',');
-			// }
-		// }			
-	// }
-	//var_dump($jsScrape);
 foreach ($arrClean AS $url => $name) {
 	if (!in_array($url, $arrNotEmpty)) {
 		$success = false;
@@ -82,6 +32,7 @@ foreach ($arrClean AS $url => $name) {
 			$store = curl_exec($ch);
 			curl_close($ch);
 			$doc = new DOMDocument();
+			libxml_use_internal_errors(true);
 			$doc->loadHTML($store);
 			$text = $doc->textContent;
 			if ($n = strpos($text,'var g_rgAppContextData')) {
@@ -131,8 +82,6 @@ foreach ($arrClean AS $url => $name) {
 		}
 	}
 }
-
-
 //*/
 
 
