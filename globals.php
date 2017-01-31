f1b75cac9a756bdfcfd15f16f25de730ed81a3c1
Mitch Wilson <Mitch Wilson>
2017-01-31 17:51:58 -0500

    bug fixes and other optimizations

--- globals.php
+++ globals.php
@@ -1,3 +1,4 @@
 <?php
 
 $GLOBALS['csgo_item_link'] = 'http://steamcommunity.com/market/listings/730/';
+$GLOBALS['listTable'] = 'skin_list';
--- import.php
+++ import.php
@@ -7,19 +7,15 @@ ini_set("allow_url_fopen", 1);
 //$json = file_get_contents('csgo-skins.txt');
 $arrSkins = json_decode($json);
 foreach ($arrSkins->items AS $skin) {
-	$name = $conn_market->real_escape_string($skin->market_name);
+	$name = $actions->functions->str_clean($skin->market_name);
 	$url = urlencode($name);
 	$url = str_replace('+','%20',$url);
 	if ((substr($url, -3) == '%29' || substr($url,0,9) != '%E2%98%85') && !strpos($name, 'Souvenir')) {  // These cases should separate out only non souvenir gun skins
 		$arrClean[$url] = $name;
 	} 
 }
-$arrNotEmpty = array();
-$sql = "SELECT link FROM id_ref WHERE market_id IS NOT NULL AND 30day_price IS NOT NULL AND 30day_count IS NOT NULL";
-$result = $conn_market->query($sql);
-while($row = $result->fetch_assoc()) {
-	$arrNotEmpty[] = $row['link'];
-}
+
+$arrNotEmpty = $actions->getExcluded();
 
 foreach ($arrClean AS $url => $name) {
 	if (!in_array($url, $arrNotEmpty)) {
@@ -39,7 +35,7 @@ foreach ($arrClean AS $url => $name) {
 				$a = substr($text,$n);
 				$raw = explode('var ',$a);
 				//print_r($raw);
-				$avg = 0;
+				$price = $count = $avg = 0;
 				foreach ($raw AS $var) {
 					$expl = explode('=',$var);
 					if (isset($expl[0]) && isset($expl[1])) {
@@ -61,24 +57,31 @@ foreach ($arrClean AS $url => $name) {
 									$count += $c;
 								}
 							}
-							$avg = '$'.number_format(round(($price/$count),2), 2, '.', ',');
+							if ($count != 0) {
+								$avg = number_format(round(($price/$count),2), 2, '.', ',');
+							}					
 						}
 					}	
 				}
-				$n = strpos($text,'Market_LoadOrderSpread'); 
-				$x = $n + 24;
-				$y = (strpos($text,')',$n) - 1) - $x;
-				$market_id = substr($text,$x, $y);
-				$sql = "INSERT INTO id_ref (name, link, market_id, 30day_price, 30day_count, load_dt) VALUES ('$name', '$url', $market_id, '$avg', $count, CURDATE()) ON DUPLICATE KEY UPDATE link = VALUES (link), market_id = VALUES (market_id), 30day_price = VALUES (30day_price), 30day_count = VALUES (30day_count), load_dt = CURDATE()";
-				if (!$conn_market->query($sql)) {
-					echo $conn_market->error;
-					die();
+				//echo $text;
+				if ($n = strpos($text,'Market_LoadOrderSpread')) {
+					$x = $n + 24;
+					$y = (strpos($text,')',$n) - 1) - $x;
+					$market_id = substr($text,$x, $y);
+				} else {
+					$market_id = 0;
 				}
-				$success = true;
+				
+				//echo $market_id;
+				$insert = array('link'=>"'$url'", 'name'=>"'$name'", 'market_id'=>"'$market_id'", '30day_price'=>$avg, '30day_count'=>$count, 'active'=>0, 'load_dt'=>'CURDATE()');
+				if ($actions->singleInsertUpdate($GLOBALS['listTable'],$insert)) {
+					echo "$name skin successfully inserted or updated!\n";
+					$success = true;
+				}				
 			} else {
 				echo "Page failed to load! Retrying ... \n";
 			}
-			sleep(5);
+			sleep(11);
 		}
 	}
 }
